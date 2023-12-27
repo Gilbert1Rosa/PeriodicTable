64947e1d21db17d78831903d6134c02617232771
Gilbert1Rosa <gilbertli2033@gmail.com>
2023-12-27 02:31:03 -0400

    Adding scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOMAIN=periodic-table
+
+# Copy conf
+# cp nginx.conf /etc/nginx/sites-enabled/$DOMAIN
+
+# Create NGINX domain
+mkdir /var/www/html
+
+cd ../ui
+npm run build
+mv ./build/* /var/www/html/
+rm build
+cd ../scripts
