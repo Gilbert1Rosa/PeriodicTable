c9b61aa7c80e4b9030f0cfc8114ce56a61fad1ca
Gilbert1Rosa <gilbertli2033@gmail.com>
2023-12-29 01:29:55 -0400

    Changing proxy

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = {
-    target: 'http://localhost:4444',
+    target: 'http://172.233.176.150:4444',
     changeOrigin: true
 }
 
